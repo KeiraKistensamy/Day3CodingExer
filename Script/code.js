75149bbb8b52cc66cd4fd664c2c6877d8486e462
KeiraKistensamy <Keirakist0987@gmail.com>
2024-05-17 15:59:39 +0200

    day3

---
+++ Script/code.js
@@ -0,0 +1,20 @@
+
+let firstName = "Keira";
+console.log(typeof firstName);
+
+let number = 42;
+console.log(typeof number);
+
+// Creating variables of each data type
+let lastName = "Kistensamy";
+console.log(typeof lastName);
+
+let num = 123;
+console.log(typeof num);
+
+let boolean = true;
+console.log(typeof boolean);
+
+let info = { name: "John", age: 30 };
+console.log(typeof info);
+
